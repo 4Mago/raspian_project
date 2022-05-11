39acc650038d8d07d28cb4a5ca86b5b437cc6f25
blixt <teodor.blixt@gmail.com>
2022-05-11 21:23:40 +0200

    boom

---
+++ server/app.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import fetch from 'node-fetch'
+import cors from 'cors'
+
+const app = express()
+app.use(cors({ origin: '*' }))
+app.get('/sl-data', function (req, res) {
+	fetch(
+		'https://api.sl.se/api2/realtimedeparturesV4.json?key=24e8694550204651b656e322387bf0bc&siteid=1365&timewindow=5',
+		{ method: 'GET' }
+	)
+		.then(response => response.json())
+		.then(data => {
+			res.send(data)
+		})
+})
+
+const port = process.env.PORT || 8080
+
+app.listen(port, function () {
+	console.log('Express server listening on port ' + port)
+})
