dcb5fa2c758ac6819efe6f7e868f42adc9ad8968
4Mago <47850942+4Mago@users.noreply.github.com>
2022-05-12 13:58:20 +0200

    klarr

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,42 @@
 import { QueryClient, QueryClientProvider } from 'react-query'
 import UpRight from './boxes/upright'
+import styled from 'styled-components'
 import './App.css'
 
 const queryClient = new QueryClient()
 
 function App() {
 	return (
+		<>
+		<Body>
+		<UpContainer>
+
 		<QueryClientProvider client={queryClient}>
 			<UpRight />
 		</QueryClientProvider>
+
+		</UpContainer>
+		</Body>
+		</>
 	)
 }
 
 export default App
+
+const Body = styled.div`
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: darkblue;
+`
+
+const UpContainer = styled.div`
+	width:60%;
+	height: 100%;
+	/* display: flex;
+	align-items: center;
+	justify-content: center; */
+	background-color: #00009B;
+`
--- client/src/boxes/upright.jsx
+++ client/src/boxes/upright.jsx
@@ -1,14 +1,61 @@
 import React from 'react'
 import { useQuery } from 'react-query'
 import { getData } from '../utils/getData'
+import styled from 'styled-components'
+
 const UpRight = () => {
 	const query = useQuery('sl-data', getData)
-	console.log(query)
+	let displayTime = ''
+	let lineNumber = ''
+	let destination = ''
+
+	if (query.isSuccess == false) {
+		console.log('something went wrong')
+	}
+
+	else {
+
+		for (let x=0; x<query.data.ResponseData.Buses.length; x++){
+			let displayTime = JSON.stringify(query.data.ResponseData.Buses[x].DisplayTime)
+			let lineNumber = JSON.stringify(query.data.ResponseData.Buses[x].LineNumber)
+			let destination = JSON.stringify(query.data.ResponseData.Buses[x].Destination)
+
+		
+
 	return (
-		<>
-			<p>Wake up it's time for school</p>
-		</>
-	)
+	<Container>
+			<>
+			<ul>
+			{query.data.ResponseData.Buses.map(function(d, idx){
+				return (<>
+				<Li key={idx}>{`linje ${d.LineNumber} mot ${d.Destination} går om ${d.DisplayTime}`}</Li>
+				</>
+			)})}
+			</ul>
+			</>
+	</Container>	
+		)}
+		console.log(displayTime)
+		console.log(query)
+	}
 }
 
+
 export default UpRight
+
+const Container = styled.div`
+	height: 100%;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`
+
+const Li = styled.li`
+	margin: 5px;
+	padding: 5px;
+	color: white;
+	font-weight: 500;
+	text-decoration: none;
+	list-style: none;
+	`
